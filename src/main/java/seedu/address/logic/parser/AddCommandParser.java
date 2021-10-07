0fdf9242c7b44219b8c0ccdd2e82135fa7241776
Alyssa <alyssa_savier@yahoo.com>
2021-10-07 22:16:21 +0800

    Implement New Add Command

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -6,17 +6,16 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PASSPORT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_STAFF;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ROOM;
 
 import java.util.Set;
 import java.util.stream.Stream;
 
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
+import seedu.address.model.person.*;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -33,20 +32,66 @@ public class AddCommandParser implements Parser<AddCommand> {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
 
-        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)
-                || !argMultimap.getPreamble().isEmpty()) {
+        if (arePrefixesPresent(argMultimap, PREFIX_PASSPORT, PREFIX_STAFF) || !argMultimap.getPreamble().isEmpty()) {
+            //think of new error msg for only can have either passport number or staffid
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         }
 
+        if (!arePrefixesPresent(argMultimap, PREFIX_PASSPORT) && !arePrefixesPresent(argMultimap, PREFIX_STAFF)) {
+            // new error msg for needing passport number OR staff id
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        }
+
+        if (arePrefixesPresent(argMultimap, PREFIX_PASSPORT)) {
+            if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_PASSPORT, PREFIX_ROOM, PREFIX_EMAIL)
+                    || !argMultimap.getPreamble().isEmpty()) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+            }
+            Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
+            Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
+            Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+            RoomNumber roomNumber = ParserUtil.parseRoomNumber(argMultimap.getValue(PREFIX_ROOM).get());
+            PassportNumber passport = ParserUtil.parsePassportNumber(argMultimap.getValue(PREFIX_PASSPORT).get());
+
+            Guest guest = new Guest(name, email, tagList, roomNumber, passport);
+            return new AddCommand(guest);
+        }
+
+        if (arePrefixesPresent(argMultimap, PREFIX_STAFF)) {
+            if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_STAFF, PREFIX_PHONE)
+                || !argMultimap.getPreamble().isEmpty()) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+            }
+            Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
+            Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
+            Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+            Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
+            StaffId staffId = ParserUtil.parseStaffId(argMultimap.getValue(PREFIX_STAFF).get());
+            Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get());
+
+            Staff staff = new Staff(name, email, tagList, address, staffId, phone);
+            return new AddCommand(staff);
+        }
+
+//
+//        Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
+//        Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get());
+//        Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
+//        Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
+//        Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+//
+//        Person person = new Person(name, phone, email, address, tagList);
+//
+//        return new AddCommand(person);
         Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
-        Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get());
         Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
-        Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
         Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+        Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
+        StaffId staffId = ParserUtil.parseStaffId(argMultimap.getValue(PREFIX_STAFF).get());
+        Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get());
 
-        Person person = new Person(name, phone, email, address, tagList);
-
-        return new AddCommand(person);
+        Staff staff = new Staff(name, email, tagList, address, staffId, phone);
+        return new AddCommand(staff);
     }
 
     /**
--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -7,7 +7,10 @@ public class CliSyntax {
 
     /* Prefix definitions */
     public static final Prefix PREFIX_NAME = new Prefix("n/");
+    public static final Prefix PREFIX_PASSPORT = new Prefix("pn/");
+    public static final Prefix PREFIX_STAFF = new Prefix("sid/");
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
+    public static final Prefix PREFIX_ROOM = new Prefix("r/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
