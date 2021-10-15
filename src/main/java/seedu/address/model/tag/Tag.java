ef2c11ef51654aa85d9c7c02a2f81fd7e5c2c6ab
calvintanwj <calvintanwj127@gmail.com>
2021-10-15 14:33:36 +0800

    Fix typical ab

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -15,7 +15,7 @@ import seedu.address.model.person.Person;
 public class Tag {
 
     public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric";
-    public static final String VALIDATION_REGEX = "\\p{Alnum}+";
+    public static final String VALIDATION_REGEX = "^[\\w\\-\\s]+$";
 
     public final String tagName;
     private List<Person> taggedPeople;
