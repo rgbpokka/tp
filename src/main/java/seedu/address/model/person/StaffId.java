7327d9b9d8ca9c10d75b649f7d7f33150278faae
Alyssa <alyssa_savier@yahoo.com>
2021-10-15 23:06:23 +0800

    Edit AddCommandParserTest

--- src/main/java/seedu/address/model/person/StaffId.java
+++ src/main/java/seedu/address/model/person/StaffId.java
@@ -6,7 +6,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class StaffId extends UniqueIdentifier {
 
     public static final String MESSAGE_CONSTRAINTS = "StaffID must be alphanumeric.";
-    public static final String VALIDATION_REGEX = "^[\\w]+";
+    public static final String VALIDATION_REGEX = "^[\\p{Alnum}]+$";
 
     public final String value;
 
--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -20,11 +20,11 @@ public class AddCommandParserTest {
 
     @Test
     public void parse_allFieldsPresent_success() {
-        Staff expectedPerson = new StaffBuilder(BOB).withTags(VALID_TAG_FRIEND).build();
+        Staff expectedPerson = new Staff(BOB.getName(), BOB.getEmail(), BOB.getTags(), BOB.getAddress(), new StaffId("123"), BOB.getPhone());
 
         // whitespace only preamble
         assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND + STAFF_ID_DESC_BOB, new AddCommand(expectedPerson));
+                + ADDRESS_DESC_BOB + TAG_DESC_FRIEND + TAG_DESC_HUSBAND + STAFF_ID_DESC_BOB, new AddCommand(expectedPerson));
 
         // multiple names - last name accepted
         assertParseSuccess(parser, NAME_DESC_AMY + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
