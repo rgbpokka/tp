60eea858f6dedd7b8c9b309fa86f6cca601f948b
calvintanwj <calvintanwj127@gmail.com>
2021-10-20 00:57:36 +0800

    Implements filter command

---
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -0,0 +1,54 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.person.TagContainsKeywordsPredicate;
+import seedu.address.model.tag.Tag;
+
+import java.util.function.Predicate;
+
+/**
+ * Finds and lists all persons in address book whose name contains any of the argument keywords.
+ * Keyword matching is case insensitive.
+ */
+public class FilterCommand extends Command {
+
+    public static final String COMMAND_WORD = "filter";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Filters the list by tags: "
+            + PREFIX_TAG + "TAG" + "...\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_TAG + "Staff "
+            + PREFIX_TAG + "Vaccinated "
+            + "\n This filters the list by those that are vaccinated and are Staff.\n";
+
+    private final TagContainsKeywordsPredicate predicate;
+    
+    public FilterCommand(TagContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPersonList(predicate);
+        model.updateFilteredTagList(getTagPredicate(predicate));
+        return new CommandResult(
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+    }
+    
+    private Predicate<Tag> getTagPredicate(TagContainsKeywordsPredicate predicate) {
+        return tag -> predicate.getTags().contains(tag); 
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof FilterCommand // instanceof handles nulls
+                && predicate.equals(((FilterCommand) other).predicate)); // state check
+    }
+    
+}
--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_TAGS;
 
 import seedu.address.model.Model;
 
@@ -12,13 +13,14 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_SUCCESS = "Listed all persons and tags";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        model.updateFilteredTagList(PREDICATE_SHOW_ALL_TAGS);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
