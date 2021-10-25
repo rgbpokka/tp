74e768f356703231cbc700846350cd79de55219e
Nicolas Chang Weng Yew <nicolas.chang99@gmail.com>
2021-10-25 23:41:05 +0800

    Add page number, invoice header and placeholder guest details

--- src/main/java/seedu/address/logic/commands/Invoice.java
+++ src/main/java/seedu/address/logic/commands/Invoice.java
@@ -1,14 +1,17 @@
 package seedu.address.logic.commands;
 
 import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.kernel.events.PdfDocumentEvent;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.borders.Border;
 import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
+import seedu.address.InvoiceNewPageHandler;
 import seedu.address.model.guest.Guest;
 
 import java.io.BufferedReader;
@@ -52,8 +55,11 @@ public class Invoice {
     public static void generateInvoicePdf(String dest) throws IOException {
 
         // Initialize document
-        Document document = createEmptyDocument(dest);
+        PdfWriter writer = new PdfWriter(dest);
+        PdfDocument pdf = new PdfDocument(writer);
+        Document document = new Document(pdf);
 
+        pdf.addEventHandler(PdfDocumentEvent.END_PAGE, new InvoiceNewPageHandler());
         // ToDo
         Paragraph guestDetails = new Paragraph("Daniel Craner, Room 102");
         document.add(guestDetails);
@@ -65,28 +71,33 @@ public class Invoice {
         document.close();
     }
 
-    private static Document createEmptyDocument(String dest) throws IOException {
-        PdfWriter writer = new PdfWriter(dest);
-        PdfDocument pdf = new PdfDocument(writer);
-
-        return new Document(pdf);
-    }
-
     private static Table createInvoiceTable() throws IOException {
         Table table = new Table(new float[]{1, 4, 2, 1, 3});
         BufferedReader br = new BufferedReader(new FileReader(DATA));
-        String line = br.readLine();
-        processCsv(table, line, FONT_BOLD, true);
-        while ((line = br.readLine()) != null) {
-            processCsv(table, line, FONT, false);
+        String currentLine = br.readLine();
+
+        // Add header row
+        addRowToTable(table, currentLine, FONT_BOLD, true);
+
+        // Process all but last row
+        String oneLineAhead = br.readLine();
+        String twoLinesAhead = br.readLine();
+        while ((twoLinesAhead != null)) { // oneLineAhead contain last row, when loop breaks
+            currentLine = oneLineAhead;
+            oneLineAhead = twoLinesAhead;
+            twoLinesAhead = br.readLine();
+            addRowToTable(table, currentLine, FONT, false);
         }
+
+        // add total cost
+        addCostRowToTable(table, oneLineAhead, FONT);
         br.close();
 
         return table;
     }
 
     // https://kb.itextpdf.com/home/it7kb/examples
-    private static void processCsv(Table table, String line, PdfFont font, boolean isHeader) {
+    private static void addRowToTable(Table table, String line, PdfFont font, boolean isHeader) {
         StringTokenizer tokenizer = new StringTokenizer(line, ",");
         while (tokenizer.hasMoreTokens()) {
             if (isHeader) {
@@ -97,6 +108,24 @@ public class Invoice {
                 table.addCell(
                         new Cell().add(
                                 new Paragraph(tokenizer.nextToken()).setFont(font)));
+            }
+        }
+    }
+
+    // https://stackoverflow.com/questions/41607891/itext-7-borderless-table-no-border
+    private static void addCostRowToTable(Table table, String line, PdfFont font) {
+        StringTokenizer tokenizer = new StringTokenizer(line, ",");
+        while (tokenizer.hasMoreTokens()) {
+            String currentToken = tokenizer.nextToken();
+            if (!tokenizer.hasMoreTokens()) {
+                Cell cell = new Cell();
+                cell.add(new Paragraph(currentToken).setFont(font));
+                table.addCell(cell);
+            } else {
+                Cell cell = new Cell();
+                cell.add(new Paragraph("fluff").setFont(font));
+                cell.setBorder(Border.NO_BORDER);
+                table.addCell(cell);
             }
         }
     }
